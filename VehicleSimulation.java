195e5e2938393a5e6774d4e8f43ed128e1c5668a
MindokRuvaak <cole_daniel@rocketmail.com>
2024-02-26 10:50:50 +0100

    forgot to commit and oush

--- VehicleSimulation.java
+++ VehicleSimulation.java
@@ -16,16 +16,16 @@ public class VehicleSimulation {
     private class VisualItem {
         protected int x;
         protected int y;
-        private final int width;
-        private final int height;
+        protected final int width;
+        protected final int height;
         private final String imagePath;
 
-        protected VisualItem(int x, int y, String imagePath) {
+        protected VisualItem(int x, int y, String imagePath, int width, int height) {
             this.x = x;
             this.y = y;
-            width = 100;
-            height = 100;
             this.imagePath = imagePath;
+            this.width = width;
+            this.height = height;
         }
 
         public int getX() {
@@ -37,12 +37,38 @@ public class VehicleSimulation {
         }
 
         public boolean overlaps(VisualItem other) {
-            return false; // TODO: 
+            return (this.xOverlap(other.lBound(), other.rBound()))
+                    && (this.yOverlap(other.tBound(), other.bBound()));
         }
-        /* x,y ---  */
+
+        private boolean yOverlap(int otherTop, int otherBottom) {
+            return (otherTop <= this.tBound() && this.tBound() <= otherBottom)
+                    || (otherTop <= this.bBound() && this.bBound() <= otherBottom);
+        }
+
+        private boolean xOverlap(int otherLeft, int otherRight) {
+            return (otherLeft <= this.lBound() && this.lBound() <= otherRight)
+                    || (otherLeft <= this.rBound() && this.rBound() <= otherRight);
+        }
 
         public String getImagePath() {
             return imagePath;
+        }
+
+        public int rBound() {
+            return x + width;
+        }
+
+        public int lBound() {
+            return x;
+        }
+
+        public int tBound() {
+            return y;
+        }
+
+        public int bBound() {
+            return y + height;
         }
     }
 
@@ -50,7 +76,7 @@ public class VehicleSimulation {
         private final IsVehicle vehicle;
 
         VisualVehicle(IsVehicle vehicle, String imagePath) {
-            super((int) vehicle.getPosition().getX(), (int) vehicle.getPosition().getY(), imagePath);
+            super((int) vehicle.getPosition().getX(), (int) vehicle.getPosition().getY(), imagePath, 100, 60);
             this.vehicle = vehicle;
         }
 
@@ -64,16 +90,20 @@ public class VehicleSimulation {
         }
     }
 
-    private class VisualWorkshop<T extends IsVehicle> extends VisualItem  {
+    private class VisualWorkshop<T extends IsVehicle> extends VisualItem {
         private final Workshop<T> workshop;
 
         VisualWorkshop(Workshop<T> workshop, int x, int y, String imagePath) {
-            super(x, y, imagePath);
+            super(x, y, imagePath, 101, 96);
             this.workshop = workshop;
         }
 
         public Workshop<T> getWorkshop() {
             return workshop;
+        }
+
+        public void openStorage() {
+            workshop.openStorage();
         }
     }
 
@@ -84,7 +114,7 @@ public class VehicleSimulation {
     private VehicleController controller;
 
     // The delay (ms) corresponds to 20 updates a sec (hz)
-    private final int delay = 10; //50;
+    private final int delay = 10; // 50;
     // The timer is started with a listener (see below) that executes the statements
     // each step between delays.
     private Timer timer; // move
@@ -107,15 +137,15 @@ public class VehicleSimulation {
         init();
     }
 
-
     private void addVehicle(IsVehicle vehicle) {
-        vehicles.add(new VisualVehicle(vehicle, "pics/"+vehicle.getModel()+".jpg"));
+        vehicles.add(new VisualVehicle(vehicle, "pics/" + vehicle.getModel() + ".jpg"));
     }
 
     private void init() {
         for (VisualVehicle visualVehicle : vehicles) {
             initVehicle(visualVehicle);
         }
+        volvoWorkshop.openStorage();
         drawVisualItem(volvoWorkshop);
     }
 
@@ -127,7 +157,6 @@ public class VehicleSimulation {
     private void addVehicleToControll(VisualVehicle v) {
         controller.addVehicle(v.getVehicle());
     }
-
 
     private void drawVisualItem(VisualItem item) {
         view.addItem(item.getX(), item.getY(), item.getImagePath());
@@ -149,46 +178,50 @@ public class VehicleSimulation {
     }
 
     public void update() {
+        VisualVehicle enteredWorkshop = null;
         for (VisualVehicle v : vehicles) {
             controller.update(v.getVehicle());
             v.update();
-            workshopInteraction(v);
+            if(workshopInteraction(v)) {
+                enteredWorkshop = v;
+            }
             drawVisualItem(v);
         }
+        vehicles.remove(enteredWorkshop);
         drawVisualItem(volvoWorkshop);
         view.update();
     }
 
-
-    private void workshopInteraction(VisualVehicle v) {
+    private boolean workshopInteraction(VisualVehicle v) {
         if (collisionOccurs(v, volvoWorkshop)) {
-        enterWorkshopIfAllowed(v.getVehicle(), volvoWorkshop.getWorkshop());
-      }
+            return enterWorkshopIfAllowed(v.getVehicle(), volvoWorkshop.getWorkshop());
+        }
+        return false;
     }
 
     private boolean collisionOccurs(VisualItem a, VisualItem b) {
         return a.overlaps(b);
     }
 
-
-    private void enterWorkshopIfAllowed(IsVehicle v, Workshop<? extends IsVehicle> w) {
+    private boolean enterWorkshopIfAllowed(IsVehicle v, Workshop<IsVolvo> w) {
         if (canEnter(v, w)) {
             enterWorkshop(v, w);
+            return true;
         }
+        return false;
     }
 
-
-    private void enterWorkshop(IsVehicle v, Workshop<? extends IsVehicle> w) {
-        
+    private void enterWorkshop(IsVehicle v, Workshop<IsVolvo> w) {
+        System.out.println("A " + v.getModel() + " has entered the workshop for service!");
+        controller.removeVehicle(v);
+        w.storeThing((IsVolvo)v);
     }
-
 
     private boolean canEnter(IsVehicle v, Workshop<? extends IsVehicle> w) {
         return v instanceof IsVolvo && w.isStorageOpen();
     }
 
-
-    public static void main(String[] args) { 
+    public static void main(String[] args) {
         VehicleSimulation vSim = new VehicleSimulation();
         // Start the timer
         vSim.start();
