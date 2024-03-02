bce44053ce67dc8fd7b26b7b16203d6220631cab
Nikhil Olsson Mukhopadhyay <gusnikhol@gu.student.se>
2024-03-02 21:48:39 +0100

    move all default package items into a dedicated application apckage

--- src/Application/VehicleSimulation.java
+++ src/Application/VehicleSimulation.java
@@ -1,3 +1,5 @@
+package Application;
+
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
--- src/Application/VehicleUI.java
+++ src/Application/VehicleUI.java
@@ -1,3 +1,5 @@
+package Application;
+
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JSpinner;
