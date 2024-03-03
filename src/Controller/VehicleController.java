ab7b9548a2d988c45247f7fa966de57918729e0c
Nikhil Olsson Mukhopadhyay <gusnikhol@gu.student.se>
2024-03-03 18:10:14 +0100

    changed an outdated comment

--- src/Controller/VehicleController.java
+++ src/Controller/VehicleController.java
@@ -1,14 +1,8 @@
 package Controller;
-import java.util.ArrayList;
-
-import Model.ModelListener;
 import Model.VehicleSimulation;
-import Model.Vehicles.*;
 
 /*
 * This class represents the Controller part in the MVC pattern.
-* It's responsibilities is to listen to the View and responds in a appropriate manner by
-* modifying the model state and the updating the view.
  */
 
 public class VehicleController {
