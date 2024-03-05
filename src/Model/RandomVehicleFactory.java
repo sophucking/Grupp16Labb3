a95de6b10bf0d24cdfac12b42f7ac275a41ed799
Nikhil Olsson Mukhopadhyay <gusnikhol@gu.student.se>
2024-03-05 14:00:42 +0100

    fixed bug where cars spawn partially outside world

--- src/Model/RandomVehicleFactory.java
+++ src/Model/RandomVehicleFactory.java
@@ -5,13 +5,15 @@ import Model.Vehicles.*;
 // A class to generate random cars. Makes dependencies nicer than putting this in Simulation
 public class RandomVehicleFactory {
     public IsVehicle makeRandomVehicle(double vehicleWidth, double vehicleHeight, double worldWidth, double worldHeight) {
+        double vehicleXPos = Math.random() * (worldWidth - vehicleWidth);
+        double vehicleYPos = Math.random() * (worldHeight - vehicleHeight);
         switch ((int) (Math.random() * 3)) {
             case 0:
-                return new Volvo240(Math.random() * worldWidth, Math.random() * worldHeight, vehicleWidth, vehicleHeight);
+                return new Volvo240(vehicleXPos, vehicleYPos, vehicleWidth, vehicleHeight);
             case 1:
-                return new Saab95(Math.random() * worldWidth, Math.random() * worldHeight, vehicleWidth, vehicleHeight);
+                return new Saab95(vehicleXPos, vehicleYPos, vehicleWidth, vehicleHeight);
             case 2:
-                return new ScaniaV8<Cargo>(Math.random() * worldWidth, Math.random() * worldHeight, vehicleWidth, vehicleHeight);
+                return new ScaniaV8<Cargo>(vehicleXPos, vehicleYPos, vehicleWidth, vehicleHeight);
             default:
                 throw new RuntimeException("Factory method failed to generate valid case");
         }
