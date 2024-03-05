8c4965383e3416f093e79e0500d5f309d6aef04d
Nikhil Olsson Mukhopadhyay <gusnikhol@gu.student.se>
2024-03-05 13:32:29 +0100

    add relevant arguments to vehicleUI constructor and refactor default values in Application constructor

--- src/Application/Application.java
+++ src/Application/Application.java
@@ -9,14 +9,16 @@ import Model.Vehicles.*;
 
 public class Application {
     public static void main(String[] args) {
+        int defaultWidth = 100;
+        int defaultHeight = 100;
         VehicleSimulation vSim = new VehicleSimulation(10);
-        vSim.addVehicle(new Volvo240(0, 0, 100, 100));
-        vSim.addVehicle(new Saab95(0, 100, 100, 100));
-        vSim.addVehicle(new ScaniaV8<Cargo>(0, 200, 100, 100));
-        vSim.addVehicle(new Volvo240(0, 300, 100, 100));
+        vSim.addVehicle(new Volvo240(0, 0, defaultWidth, defaultHeight));
+        vSim.addVehicle(new Saab95(0, 100, defaultWidth, defaultHeight));
+        vSim.addVehicle(new ScaniaV8<Cargo>(0, 200, defaultWidth, defaultHeight));
+        vSim.addVehicle(new Volvo240(0, 300, defaultWidth, defaultHeight));
         VehicleView vehicleView = new VehicleView("CarSim 0.4", 800,400, vSim);
         VehicleController controller = new VehicleController(vSim);
-        VehicleUI vehicleUI = new VehicleUI(vehicleView, controller);
+        VehicleUI vehicleUI = new VehicleUI(vehicleView, controller, defaultWidth, defaultHeight);
         vehicleUI.initWidgets();
         vSim.addModelListener(vehicleView);
         vehicleView.initItems();
